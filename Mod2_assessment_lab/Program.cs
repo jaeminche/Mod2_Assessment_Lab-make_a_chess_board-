7e70d71c202fa30e20f7923fa4f6a20488d42b1c
jaeminche <jaeminche@gmail.com>
2017-09-03 20:33:01 +0900

    Do without a nested loop yet

--- Mod2_assessment_lab/Program.cs
+++ Mod2_assessment_lab/Program.cs
@@ -10,6 +10,29 @@ namespace Mod2_assessment_lab
     {
         static void Main(string[] args)
         {
+            string a = "XOXOXOXO";
+            string b = "OXOXOXOX";
+            for (int rowCount = 0; rowCount < 8; rowCount++)
+            {
+                //string b = for (int XOCount = 0; XOCount < 4; XOCount ++) { Console.Write(a); };
+                if ((rowCount + 2) % 2 != 0 )
+                {
+                    Console.WriteLine(b);
+                    //    //for (int XOCount = 0; XOCount < 4; XOCount++)
+                    //    //{
+                    //    Console.WriteLine(a);
+                    //    //}
+                    //}
+
+                    //Console.WriteLine(for (int letterCount = 0; letterCount < 4; letterCount++)
+                    //        {Console.Write(a);});   
+                    ////}
+                }
+                else
+                {
+                    Console.WriteLine(a);
+                }
+            }
         }
     }
 }
